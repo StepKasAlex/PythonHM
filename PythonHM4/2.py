8da193d9717311d7badb8ace68507a32e9ff4fcf
StepKasAlex <62915291+StepKasAlex@users.noreply.github.com>
2020-04-09 19:49:35 +0300

    Delete 2.py

--- PythonHM4/2.py
+++
@@ -1,7 +0,0 @@
-# Представлен список чисел. Необходимо вывести элементы 
-# исходного списка, значения которых больше предыдущего элемента.
-
-
-list_1 = [8, 2, 5, 1, 10, 23, 44, 3, 7]
-solution_list = [list_1[number] for number in range(1, len(list_1)) if list_1[number] > list_1[number - 1]]
-print(solution_list)
