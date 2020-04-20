73fc48d4cd6d4a5162682d4e98778a20781dc550
StepKasAlex <62915291+StepKasAlex@users.noreply.github.com>
2020-04-20 17:55:56 +0300

    Add files via upload

---
+++ ddd/6.py
@@ -0,0 +1,11 @@
+import itertools
+
+col_a = int(input())
+l_a = set([input() for el in range(col_a)])
+col_b = int(input())
+l_b = set([input() for el in range(col_a)])
+col_c = int(input())
+l_c = set([input() for el in range(col_a)])
+s = int(input())
+
+for l in range()
